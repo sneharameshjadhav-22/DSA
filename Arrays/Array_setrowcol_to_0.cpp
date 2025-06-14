729f1297e13c0a731e26c0ddd3ca388ff3050626
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-06-14 19:36:08 +0530

    Update Array_setrowcol_to_0.cpp

--- Arrays/Array_setrowcol_to_0.cpp
+++ Arrays/Array_setrowcol_to_0.cpp
@@ -1,7 +1,7 @@
-Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+/*Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 You must do it in place.
 Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
-Output: [[1,0,1],[0,0,0],[1,0,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]*/
 
 
 class Solution {
