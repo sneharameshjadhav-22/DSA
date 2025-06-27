cf669f62159afe5116567a680d44e5a0c4ea1ef5
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-06-27 12:48:52 +0530

    Update Divisors of the number.cpp

--- Arrays/Divisors of the number.cpp
+++ Arrays/Divisors of the number.cpp
@@ -12,14 +12,15 @@ Explanation: The divisors of 8 are 1, 2, 4, 8.*/
 class Solution {
 public:
     vector<int> divisors(int n) {
-        vector<int>result;
-        for(int i=1; i<=n; i++){
-            if(n%i==0){
+        vector<int> result;
+        for (int i = 1; i <= sqrt(n); i++) {
+            if (n % i == 0) {
                 result.push_back(i);
+                if (n / i != i) {
+                    result.push_back(n / i);
+                }
             }
-
         }
         return result;
-
     }
 };
