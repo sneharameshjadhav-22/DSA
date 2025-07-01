83e3a5389609097f7dd18b6b9e4655c023e58873
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-07-01 20:57:28 +0530

    Create a missing number.cpp

---
+++ Arrays/a missing number.cpp
@@ -0,0 +1,40 @@
+/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+Input: nums = [0,1]
+Output: 2*/
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum= (n*(n+1)) / 2;
+        int sum1=0;
+        for(int i=0; i<n; i++){
+            sum1=sum1+nums[i];
+        }
+        return sum-sum1;
+        
+    }
+};
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int xor1= 0;
+        int xor2=0;
+        for(int i=0; i<n; i++){
+            xor2=xor2^nums[i];
+            xor1=xor1 ^ i;
+        }
+        xor1= xor1 ^ n;
+        return xor1 ^ xor2;
+    }
+};
