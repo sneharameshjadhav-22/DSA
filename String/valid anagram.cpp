bf21e0f0b419ee89e4064833662bfa35f5e9c325
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-07-13 14:32:02 +0530

    Create valid anagram.cpp

---
+++ String/valid anagram.cpp
@@ -0,0 +1,20 @@
+/*Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+Input: s = "rat", t = "car"
+Output: false*/
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+        
+    }
+};
+
+ 
