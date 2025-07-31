7cb6cdb033240b72e60a3b79d3d32b69004ca7c5
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-07-31 10:52:27 +0530

    Create insert a node at end.cpp

---
+++ LinkedList/insert a node at end.cpp
@@ -0,0 +1,49 @@
+/*Given the head of a Singly Linked List and a value x, insert that value x at the end of the LinkedList and return the modified Linked List.
+Examples :
+Input: LinkedList: 1->2->3->4->5 , x = 6
+Output: 1->2->3->4->5->6
+Explanation: 
+We can see that 6 is inserted at the end of the linkedlist.
+Input: LinkedList: 5->4 , x = 1
+Output: 5->4->1
+Explanation: 
+We can see that 1 is inserted at the end of the linkedlist.
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+Constraints:
+0 <= number of nodes <= 105
+1 <= node->data , x <= 103*/
+
+
+
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution {
+  public:
+    Node * insertAtEnd(Node *head, int x) {
+        
+        // Code here
+        Node *newhead=new Node(x);
+        if(head==NULL){
+            return newhead;
+        }
+        
+        Node *temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+            
+        }
+        temp->next=newhead;
+        newhead->next=NULL;
+        return head;  
+    }
+};
+
