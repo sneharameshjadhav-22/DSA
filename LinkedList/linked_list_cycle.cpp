ab931f70bec3ad7054a2a6427f74b042599f4626
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-08-01 10:23:39 +0530

    Update linked_list_cycle.cpp

--- LinkedList/linked_list_cycle.cpp
+++ LinkedList/linked_list_cycle.cpp
@@ -25,3 +25,43 @@ public:
         return false;
     }
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL ){
+            return false;
+        }
+        if(head->next==NULL){
+            return false;
+
+        }
+        ListNode *slow=head;
+        ListNode *fast=head;
+       
+        while(fast && fast->next){
+            
+            fast=fast->next->next;
+            slow=slow->next;
+            
+             if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+        
+
+
+        
+    }
+};
