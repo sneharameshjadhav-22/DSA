918d5199630fda902b7968e7b1fe63593aa6b81a
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-08-07 10:44:00 +0530

    Create remove linked list.cpp

---
+++ LinkedList/remove linked list.cpp
@@ -0,0 +1,78 @@
+
+/*Given the head of a linked list, return the list after sorting it in ascending order.
+
+
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+ 
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL){
+            return NULL;
+        }
+        
+        while(head!=NULL && head->val==val){
+               ListNode*n1=head;
+               head=head->next;
+               delete n1;
+        }
+        ListNode* temp=head;
+    
+    
+        while(temp!=NULL && temp->next!=NULL){
+        
+            if(temp->next->val==val){
+                ListNode* curr=temp->next;
+                temp->next=temp->next->next;
+
+                delete curr;
+                   
+            }
+            
+            else{
+                temp=temp->next;
+            }  
+            
+        } 
+       
+        return head;
+
+        
+    }
+};
