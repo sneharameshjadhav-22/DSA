c11caecbcf9a3f658a95b98d8b7b57e1f0d2955f
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-09-04 10:17:07 +0530

    Update Fibonacci number.cpp

--- Recursion /Fibonacci number.cpp
+++ Recursion /Fibonacci number.cpp
@@ -24,3 +24,28 @@ public:
         
     }
 };
+
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to return list containing first n fibonacci numbers.
+    int fibo(int n){
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }else{
+            return fibo(n-2) + fibo(n-1);
+        }
+    }
+    vector<int> fibonacciNumbers(int n) {
+        // code here
+       vector<int>ans;
+       for(int i=0; i<n; i++){
+           ans.push_back(fibo(i));
+       }
+       return ans;
+    }
+};
