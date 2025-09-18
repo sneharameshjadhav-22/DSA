90ba1c303d9904ec1b4573640c908c2ba897c9a5
Sneha Ramesh Jadhav <sneharameshjadhav22@gmail.com>
2025-09-18 11:56:08 +0530

    Update combination.cpp

--- Recursion /combination.cpp
+++ Recursion /combination.cpp
@@ -42,3 +42,27 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    void fun( int curr, int n, int k, vector<int>&ans, vector<vector<int>>&res){
+        if(ans.size()==k){
+            res.push_back(ans);
+            return;
+        }
+        for(int i=curr; i<=(n-(k-ans.size())+1); i++){
+            ans.push_back(i);
+            fun(i+1, n, k, ans, res);
+            ans.pop_back();    
+        }
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>ans;
+        vector<vector<int>>res;
+        fun( 1, n, k, ans, res);
+        return res;
+        
+    }
+};
